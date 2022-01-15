337874691cc405b462608c50c95c996b5a7cccbe
BioLawyer <97779034+BioLawyer@users.noreply.github.com>
2022-01-15 09:54:59 +0200

    Add files via upload

---
+++ task003.js
@@ -0,0 +1,14 @@
+"use strict"
+
+if(val == 'a'){
+  console.log('a');
+ }
+else if(val == 'b' ||
+        val == 'c' ||
+        val == 'd' ||
+        val == 'e'){
+  console.log('others');
+ }
+else{
+  console.log('unknown')
+ }
