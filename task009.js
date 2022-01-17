84e0cb440874a92e29917450f146ef80ca765e30
BioLawyer <97779034+BioLawyer@users.noreply.github.com>
2022-01-17 11:58:07 +0200

    Update task009.js

--- task009.js
+++ task009.js
@@ -1,11 +1,42 @@
 "use strict"
 
+// поскольку по условиям задания на входе получем массив, уже отсортированный по возрастанию,
+// то для получения массива, отсортированного по убыванию
+// можем просто по очереди скопировать все элементы из исходного во временный массив в обратном порядке
+
 let arr = [1,2,3,4,5,6];
 
-let reversedArr = [];
+let revArr = [];
 
-for(let i = 0; i < arr.length; i++){
-  reversedArr[arr.length - (i + 1)] = arr[i];
+let arrIndex = arr.length - 1;
+let revArrIndex = 0;
+
+for(; arrIndex > 0; arrIndex--, revArrIndex++){
+  revArr[revArrIndex] = arr[arrIndex];
  }
 
-console.log(reversedArr);
+arr = revArr;
+
+console.log(arr);
+
+// это решение для случаев, когда входящий массив не был отсортирован по возрастанию
+
+let arr = [1,2,3,4,5,6];
+
+let lastIndex = arr.length;
+
+for(; lastIndex > 0; lastIndex--){
+  let currentIndex = 0;
+  let nextIndex = 1;
+
+  for(; currentIndex < lastIndex; currentIndex++, nextIndex++){
+    if(arr[currentIndex] < arr[nextIndex]){
+
+      let greater = arr[nextIndex];
+      arr[nextIndex] = arr[currentIndex];
+      arr[currentIndex] = greater;
+    }
+  }
+}
+
+console.log(arr);
