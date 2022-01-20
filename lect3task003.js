90b3cc22d664ae46023769e4575821a2483660ee
BioLawyer <97779034+BioLawyer@users.noreply.github.com>
2022-01-20 10:59:04 +0200

    Add files via upload

---
+++ lect3task003.js
@@ -0,0 +1,11 @@
+"use strict"
+
+function f(n1, n2, n3) {
+  for (let arg of arguments) {
+    if (String(arg) == 'NaN' || typeof arg != "number") {
+      throw new Error ('parameter type is not a Number');
+    }
+  }
+  
+  return (n1 - n2) / n3;
+}
