072ab3da3c3480f75edbb4ff4028450431d6f0f2
BioLawyer <97779034+BioLawyer@users.noreply.github.com>
2022-01-20 11:00:23 +0200

    Update lect3task003.js

--- lect3task003.js
+++ lect3task003.js
@@ -2,8 +2,8 @@
 
 function f(n1, n2, n3) {
   for (let arg of arguments) {
-    if (String(arg) == 'NaN' || typeof arg != "number") {
-      throw new Error ('parameter type is not a Number');
+    if (String(arg) == 'NaN' || typeof arg != 'number') {
+      throw new Error ('all parameters type should be a Number');
     }
   }
   
